fca25ad55b511e0ad4a887a81d38759b9c2466c3
Benga <benga.olasebikan@gmail.com>
2021-01-22 13:06:28 +0000

    Challenges created

---
+++ index.js
@@ -0,0 +1,51 @@
+// CHALLENGE 1: Return the factorial of the provided integer.
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+// Factorials are often represented with the shorthand notation n!
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+// Only integers greater than or equal to zero will be supplied to the function.
+
+function factorialize(num) {
+
+}
+
+factorialize(5)
+
+// CHALLENGE 2
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+
+function repeatStringNumTimes(str, num) {
+
+}
+
+repeatStringNumTimes('abc', 3)
+
+// CHALLENGE 3
+// The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+
+// You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+
+function convertToF(celsius) {
+  
+}
+
+convertToF(30)
+
+// CHALLENGE 4
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+function findElement(arr, func) {
+
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0)
+
+// CHALLENGE 5
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+function largestOfFour(arr) {
+
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
